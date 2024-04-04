d5dc14e3f6205360378fa1ecfb4500d6a67bf63c
Mark-shija <plusmaxtechtz@gmail.com>
2024-04-04 22:38:12 +0300

    variables in python

---
+++ variable.py
@@ -0,0 +1,14 @@
+num = 10
+num1 = num2 = 100
+print(num2)
+
+num2 = 'Now contain string';
+print(num2);
+
+a = b = c = 100
+print(a, b, c)
+
+type(num2)
+# Output: <class 'str'>
+
+print(type(num2))
