7f7ae31a81084945e58c68138da737301e0eb773
Mark-shija <plusmaxtechtz@gmail.com>
2024-04-04 22:47:21 +0300

    variables in python

--- variable.py
+++ variable.py
@@ -12,3 +12,15 @@ type(num2)
 # Output: <class 'str'>
 
 print(type(num2))
+
+# GARBAGE ALLOCATION OF MEMORY
+# consider
+
+a  = b = 300
+b = 200
+a = 400
+print("Memory Address of a : ", id(a))
+print("Memory Address of b : ", id(b))
+
+# we lost access to 300 object
+
